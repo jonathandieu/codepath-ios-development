e35a6f04275b64f394c0fc13955387433797bda9
Jonathan Dieu <jonathanhuydieu@gmail.com>
2023-08-26 11:09:34 -0400

    feat: implement main view with info and background color changing button

--- CodePath-Prework/ViewController.swift
+++ CodePath-Prework/ViewController.swift
@@ -8,12 +8,27 @@
 import UIKit
 
 class ViewController: UIViewController {
-
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func changeBackgroundColor(_ sender: UIButton) {
+        let randomColor = changeColor()
+        view.backgroundColor = randomColor
+        
+        let randomTextColor = changeColor()
+        jobLabel.textColor = randomTextColor
+    }
+    
+    @IBOutlet weak var jobLabel: UILabel!
+    
+    func changeColor() -> UIColor {
+        let red = CGFloat.random(in: 0...1)
+        let green = CGFloat.random(in: 0...1)
+        let blue = CGFloat.random(in: 0...1)
+        
+        return UIColor(red: red, green: green, blue: blue, alpha: 0.5)
+    }
 }
 
